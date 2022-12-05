f09a4e1227459161696b31e055a082f0a6d79a10
M Suhartono <85420252+msuhartono@users.noreply.github.com>
2022-12-05 11:50:14 +0700

    Add files via upload

---
+++ tampil.php
@@ -0,0 +1,66 @@
+<?php
+
+require 'fungsi.php':
+$Koneksi = new Koneksi();
+$data = $Koneksi->tampilData("SELECT * FROM siswa");
+
+if (isset($_POST['cari'])) {
+    $keyword = $_POST['keyword'];
+    $data = $Koneksi->cariData($keyword);
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h2>Tambah Data</h2>
+
+    <a href="tambahphp">Tambah</a>
+    <form method="post">
+        <input type="text" name="keyword">
+        <button type="submit" name="cari">Cari</button>
+    </form>
+
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Jurusan</th>
+            <th>Opsi</th>
+        </tr>
+
+        <?php $i = 1; ?>
+        <?php foreach ($data as $d) { ?>
+            <tr>
+                <td>
+                    <?= $i; ?>
+                </td>
+                <td>
+                    <?= $d['Nama']; ?>
+                </td>
+                <td>
+                    <?= $d['kelas']; ?>
+                </td>
+                <td>
+                <td>
+                    <a href="hapus.php?id=<?= $d["id"]; ?>">Hapus</a>
+                    <a href="edit.php?id=<?= $d["id"]; ?>">Edit</a>
+                </td>
+            </tr>
+            <?php $i+; ?>
+        <?php } ?>
+    </table>
+</body>
+
+</html>
